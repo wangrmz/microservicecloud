fcc82536290ba5636ee1d871d8a7de71a88401a3
zhurousidai <Rj613814>
2023-04-26 15:29:42 +0800

    SpringCloud微服务工程

---
+++ wrmeng-tes02/src/test/java/com/example/wrmengtes02/WrmengTes02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wrmengtes02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrmengTes02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
